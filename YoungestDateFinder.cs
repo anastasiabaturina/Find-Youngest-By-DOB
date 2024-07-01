1df0a6a0c947c995c47b5e44a505f88da9202b9d
Anastasia Baturina <a.baturina@sh.dunice.net>
2024-07-01 13:58:59 +0300

    solved task

---
+++ YoungestDateFinder.cs
@@ -0,0 +1,10 @@
+﻿namespace FindYoungestByDOB;
+
+public class YoungestDateFinder
+{
+    public static void FindYoungestDates(DateTime[] dates)
+    {
+        Array.Sort(dates);
+        Console.WriteLine($"{dates[^2]}, {dates[^1]}");
+    }
+}
